fa873ffb9332c5ea8a260da8b90ea1c363ea12b5
tain <tain@tain-E430.(none)>
2013-04-30 02:00:02 +0800

    1.add install-deb ; 2.add bash.bashrc

---
+++ mint13-install-deb.sh
@@ -0,0 +1,2 @@
+sudo apt-get -y install vim openssh-server git subversion dos2unix tree ctags cscope g++ build-essential libncurses5-dev manpages-posix manpages-posix-dev nfs-common nfs-kernel-server smbfs reiserfsprogs ibus ibus-pinyin apache2
+sudo apt-get -y install xinetd tftpd tftp
